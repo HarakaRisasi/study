2bbdd70d4cd1e61d3eb78ffc4847cd3e7c8a3934
Alexander <haraka@MacBook-Air-Alexander.local>
2024-04-29 15:23:15 +0700

    Задача: создать фрейм данных и заполнить его поля

--- pandas_create_dataframe_from_list.py
+++ pandas_create_dataframe_from_list.py
@@ -1,11 +1,13 @@
 
-"""Создать фрейм данных из списка со списками чисел, 
+"""Создать фрейм данных из списка со списками чисел,
 разместить их в фреймегде будет два поля с названиями 'student_id', 'age']"""
 
 import pandas as pd
 
 student_data = [[1, 15], [2, 11], [3, 11], [4, 20]]
 
-def createDataframe(student_data: List[List[int]]) -> pd.DataFrame:
-    return pd.DataFrame(student_data, columns=['student_id', 'age'])
 
+def createDataframe(student_data: list[list[int]]) -> pd.DataFrame:
+    return pd.DataFrame(
+        student_data, columns=['student_id', 'age']
+        )
