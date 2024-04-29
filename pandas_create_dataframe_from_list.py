cbb88e9ad58e8d2aedfe2ea1d2ff6d78cc25ee03
Alexander <haraka@MacBook-Air-Alexander.local>
2024-04-29 19:26:09 +0700

    сделал более красивым написание кода функции - вынес имена полей таблицы в отдельную переменную

--- pandas_create_dataframe_from_list.py
+++ pandas_create_dataframe_from_list.py
@@ -8,6 +8,10 @@ student_data = [[1, 15], [2, 11], [3, 11], [4, 20]]
 
 
 def createDataframe(student_data: list[list[int]]) -> pd.DataFrame:
+    column_names = ["student_id", "age"]
     return pd.DataFrame(
-        student_data, columns=['student_id', 'age']
+        student_data, columns=column_names
         )
+
+
+createDataframe(student_data)
