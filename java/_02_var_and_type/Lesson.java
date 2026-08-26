1aae5ee6f3fe4a3a905b75416c2602de12b8e293
haraka <harakarisasi@gmail.com>
2026-08-26 23:37:20 +0300

    Добавлены комментарии к примерам арифметических операций в методе main урока по переменным и типам в Java для улучшения понимания кода.

--- java/_02_var_and_type/Lesson.java
+++ java/_02_var_and_type/Lesson.java
@@ -1,9 +1,9 @@
 public class Lesson {
     public static void main(String[] args) {
-        int ex_1 = 3 * 5;
-        int ex_2 = -8 / (-4);
+        int ex_1 = 3 * 5;   // 3 * 5 = 15
+        int ex_2 = -8 / (-4); // -8 / (-4) = 2
 
-        System.out.println(ex_1);
-        System.out.println(ex_2);
+        System.out.println(ex_1); // 15
+        System.out.println(ex_2); // 2
     }
 }
