370044052391e2aad49fc5a0c4bdf7b4f4f3b2c1
haraka <harakarisasi@gmail.com>
2026-08-27 22:00:25 +0300

    Добавлены новые примеры арифметических операций и комментарии для улучшения понимания порядка вычислений в методе main урока по переменным и типам в Java.

--- java/_03_operators/Lesson.java
+++ java/_03_operators/Lesson.java
@@ -18,6 +18,16 @@ public class Lesson {
         System.out.println(3 == 4 | 3 != 4);
 
         // Вывод на экран результата выражения 3 + 4 * 5
-        System.out.println(3 + 4 * 5);        
+        System.out.println(3 + 4 * 5);     
+
+        // Понимание композиции операций (порядка вычислений) и приоритета операторов 
+        System.out.println(8 / 2 + 5 - -3 / 2);
+        System.out.println((2 + 3) * 4 - 10 / 2);
+        System.out.println(7 / 2 + 7 % 2);
+        System.out.println(100 - 20 * 3 / 2 + 5);
+        System.out.println((-5 + 3) * (8 / 3) - 2);
+
+        // среднее арифметическое трёх чисел: 10, 15, 20
+        System.out.println((10 + 15 + 20) / 3);
     }
 }
