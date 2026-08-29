86c9bc7b4a89e5c79848ee0effe8a2b4fe4a7a74
haraka <harakarisasi@gmail.com>
2026-08-30 00:00:10 +0300

    Добавлено новое задание для вывода строки с текстом о любимом слове Хала Дрого и комментарии для улучшения понимания работы со строками в методе main урока по строкам в Java.

--- java/_06_strings/Lesson.java
+++ java/_06_strings/Lesson.java
@@ -1,5 +1,7 @@
 public class Lesson {
     public static void main(String[] args) {
         // Урок 06: Строки
+        // Задание 01: Выведите на экран строку: «Khal Drogo's favorite word is «athjahakar»».
+        System.out.println("\"Khal Drogo's favorite word is \"athjahakar\"\""); // "Khal Drogo's favorite word is "athjahakar""
     }
 }
